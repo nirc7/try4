e2b8445dc70de13184d01c44765ed0680ae39eeb
nc <31476486+nirc7@users.noreply.github.com>
2023-05-31 10:15:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         ruppin2!
+         ruppin3!
         </p>
         <a
           className="App-link"
